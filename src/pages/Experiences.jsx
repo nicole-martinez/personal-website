eca4b5c677009b9d19f403d0bdb4dc4bedeb3476
Nicole Nicole <nicole.martinez@berkeley.edu>
2023-12-25 23:22:34 -0800

    Add to experience section

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -17,10 +17,21 @@ function Experiences() {
           logo={codebaseLogo}
           company="Codebase | Open Heart Kitchen"
           role="Mentored Software Developer"
-          dates="September 2023 - present"
+          dates="August 2023 - present"
           // text = "Designed and built a full stack web application for Open Heart Kitchen to organize and manage schedule employee shifts. Developed a dashboard, admin access model, and auto-assign shifts algorithm for employees. Built a mySQL and Express.js back-end for data fetching, sorting, and editing functionalities."
           text="Designed and constructed a comprehensive full-stack web application to streamline the organization/management of employee shift scheduling, featuring a user-friendly dashboard, admin access framework, and an automated shift assignment algorithm. Developed a robust backend infrastructure using MySQL and Express.js to efficiently retrieve, organize, and edit functionality, therefore ensuring the seamless execution of data management and scheduling operations."
         ></ExperienceCard>
+
+        <ExperienceCard
+          logo={ngcLogo}
+          company="NextGen Consulting | Palantir Technologies"
+          role="Senior Business Analyst"
+          dates="August 2023 - present"
+          text="Crafted and executed a comprehensive data sourcing strategy for Palantir&rsquo;s expense management, encompassing the collection
+          of market rate data and calculation of facilities management rates of various Palantir locations (US, EU, ME, APAC). Reduced costs by building an intelligence layer that identifies key heuristics for discerning market costs, then finding specific
+          levers that Palantir can take leverage of to optimize expenditure."
+        ></ExperienceCard>
+
 
         <ExperienceCard
           logo={ngcLogo}
